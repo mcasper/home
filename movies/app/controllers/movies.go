4ab4e5c02f8ed775e1230ee59f56e469f202ecdc
Matt Casper <matthewvcasper@gmail.com>
2018-11-28 10:10:54 -0800

    View and create movies

--- movies/app/controllers/movies.go
+++ movies/app/controllers/movies.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+
+	"github.com/mcasper/home/movies/app/routes"
 	"github.com/revel/revel"
 )
 
@@ -31,4 +33,43 @@ func (c Movies) Index() revel.Result {
 	}
 
 	return c.Render(movies)
+}
+
+func (c Movies) New() revel.Result {
+	return c.Render()
+}
+
+func (c Movies) Create(movie Movie) revel.Result {
+	db, connErr := gorm.Open("postgres", "postgres://localhost/movies_development?sslmode=disable")
+	db.LogMode(true)
+	if connErr != nil {
+		panic(fmt.Sprintf("failed to connect to database %v: %v", "movies_development", connErr))
+	}
+	defer db.Close()
+
+	if movieValidationErr := validateMovie(movie); movieValidationErr != nil {
+		return c.Redirect(routes.Movies.New())
+	}
+
+	if movieCreateErr := db.Create(&movie).Error; movieCreateErr != nil {
+		panic(fmt.Sprintf("encountered an error creating a movie: %v", movieCreateErr))
+	}
+
+	return c.Redirect(routes.Movies.Index())
+}
+
+type ValidationError struct {
+	s string
+}
+
+func (e *ValidationError) Error() string {
+	return e.s
+}
+
+func validateMovie(movie Movie) error {
+	if movie.Title == "" {
+		return &ValidationError{s: "Error"}
+	} else {
+		return nil
+	}
 }
