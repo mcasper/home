e46748320d9ed4179cadf6d6f30db4ca0f5f6d64
Matt Casper <matthewvcasper@gmail.com>
2018-11-30 12:23:17 -0800

    Add basic views, seed data, and url parsing and routing

--- scoreboard-frontend/src/Main.elm
+++ scoreboard-frontend/src/Main.elm
@@ -1,26 +1,180 @@
-module Main exposing (Model, Msg(..), init, main, update, view)
+module Main exposing (Model, Msg(..), Route(..), init, main, parseRoute, update, view)
 
 import Browser
 import Browser.Navigation as Nav
 import Html exposing (Html, a, div, h1, img, nav, p, text, ul)
 import Html.Attributes exposing (class, href, src, style, title)
 import Url
+import Url.Parser exposing ((</>), (<?>), Parser, int, map, oneOf, s, string, top)
+import Url.Parser.Query as Query
+
+
+
+---- ROUTES ----
+
+
+type Route
+    = MatchesIndex (Maybe String)
+    | MatchesNew
+
+
+parseRoute : Url.Url -> Maybe Route
+parseRoute url =
+    Url.Parser.parse routeParser url
+
+
+routeParser : Parser (Route -> a) a
+routeParser =
+    oneOf
+        [ map MatchesIndex (top <?> Query.string "q")
+        , map MatchesIndex (s "matches" <?> Query.string "q")
+        , map MatchesNew (s "matches" </> s "new")
+        ]
+
+
+
+---- DATA TYPES ----
+
+
+type alias Player =
+    { name : String }
+
+
+type alias Score =
+    { value : Int }
+
+
+type alias Team =
+    { players : List Player
+    , scores : List Score
+    , name : Maybe String
+    }
+
+
+type alias Match =
+    { game : String
+    , teams : List Team
+    }
 
 
 
 ---- DATA ----
+
+
+matchesSeed =
+    [ { game = "Sequence"
+      , teams =
+            [ { players =
+                    [ { name = "Matt"
+                      }
+                    ]
+              , scores =
+                    [ { value = 1 }
+                    , { value = 1 }
+                    , { value = 3 }
+                    ]
+              , name = Nothing
+              }
+            , { players =
+                    [ { name = "Ashlyn"
+                      }
+                    ]
+              , scores =
+                    [ { value = 1 }
+                    , { value = 1 }
+                    , { value = 2 }
+                    ]
+              , name = Nothing
+              }
+            ]
+      }
+    , { game = "Sequence"
+      , teams =
+            [ { players =
+                    [ { name = "Kim"
+                      }
+                    ]
+              , scores =
+                    [ { value = 1 }
+                    , { value = 1 }
+                    , { value = 3 }
+                    ]
+              , name = Nothing
+              }
+            , { players =
+                    [ { name = "Neal"
+                      }
+                    ]
+              , scores =
+                    [ { value = 1 }
+                    , { value = 1 }
+                    , { value = 2 }
+                    ]
+              , name = Nothing
+              }
+            ]
+      }
+    , { game = "Sequence"
+      , teams =
+            [ { players =
+                    [ { name = "Kim"
+                      }
+                    , { name = "Neal"
+                      }
+                    ]
+              , scores =
+                    [ { value = 1 }
+                    , { value = 1 }
+                    , { value = 3 }
+                    ]
+              , name = Just "The Parents"
+              }
+            , { players =
+                    [ { name = "Matthew"
+                      }
+                    , { name = "Ashley"
+                      }
+                    ]
+              , scores =
+                    [ { value = 1 }
+                    , { value = 1 }
+                    , { value = 2 }
+                    ]
+              , name = Just "The Awesomes"
+              }
+            , { players =
+                    [ { name = "Audrey"
+                      }
+                    , { name = "Ashlyn"
+                      }
+                    ]
+              , scores =
+                    [ { value = 1 }
+                    , { value = 1 }
+                    , { value = 2 }
+                    ]
+              , name = Just "The Girls"
+              }
+            ]
+      }
+    ]
+
+
+
 ---- MODEL ----
 
 
 type alias Model =
     { key : Nav.Key
     , url : Url.Url
+    , route : Maybe Route
+    , matches : List Match
     }
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-    ( Model key url, Cmd.none )
+    ( Model key url (parseRoute url) matchesSeed, Cmd.none )
 
 
 
@@ -45,7 +199,7 @@ update msg model =
                     ( model, Nav.load href )
 
         UrlChanged url ->
-            ( model, Cmd.none )
+            ( { model | route = parseRoute url }, Cmd.none )
 
         NoOp ->
             ( model, Cmd.none )
@@ -57,31 +211,93 @@ update msg model =
 
 view : Model -> Browser.Document Msg
 view model =
-    { title = "Home"
+    { title = titleForRoute model.route
     , body = [ viewBody model ]
     }
 
 
+titleForRoute : Maybe Route -> String
+titleForRoute route =
+    case route of
+        Just (MatchesIndex query) ->
+            "Scoreboard - Matches"
+
+        Just MatchesNew ->
+            "Scoreboard - New Match"
+
+        Nothing ->
+            "Scoreboard"
+
+
 viewBody : Model -> Html Msg
 viewBody model =
-    div []
-        [ viewNav
-        , h1 [ style "margin" "35px" ] [ text "Scoreboard" ]
-        ]
+    case model.route of
+        Just (MatchesIndex query) ->
+            div []
+                [ viewNav
+                , h1 [ style "margin" "35px" ] [ text "Scoreboard" ]
+                , viewMatches model
+                ]
+
+        Just MatchesNew ->
+            div []
+                [ viewNav
+                , h1 [ style "margin" "35px" ] [ text "New Scoreboard" ]
+                ]
+
+        Nothing ->
+            div [] [ text "Uh oh, 404!" ]
 
 
 viewNav =
     nav [ class "navbar navbar-dark bg-dark", style "height" "70px", style "color" "white" ]
         [ div []
-            [ a [ href "#", style "text-decoration" "none", style "color" "white" ] [ text "Scoreboard" ]
+            [ a [ href "/", style "text-decoration" "none", style "color" "white" ] [ text "Scoreboard" ]
             , text "\n|\n"
             , a [ href "http://localhost:3000/", style "text-decoration" "none", style "color" "white" ] [ text "Back to Home" ]
             ]
         , div []
-            [ a [ href "#", style "text-decoration" "none", style "color" "white" ]
-                [ text "Edit"
+            [ a [ href "/matches/new", style "text-decoration" "none", style "color" "white" ]
+                [ text "New"
                 ]
             ]
+        ]
+
+
+viewMatches : Model -> Html Msg
+viewMatches model =
+    ul []
+        [ div [ class "container" ] (List.map viewMatch model.matches)
+        ]
+
+
+viewMatch : Match -> Html Msg
+viewMatch match =
+    div [ class "container row border-bottom", style "min-height" "75px", style "margin-top" "10px" ]
+        [ p [ class "col text-left" ] [ text match.game ]
+        , viewTeams match.teams
+        , p [ class "col text-right" ] [ text ">" ]
+        ]
+
+
+viewTeams : List Team -> Html Msg
+viewTeams teams =
+    div [ class "col-6 container row justify-content-center" ] (List.map viewTeam teams)
+
+
+viewTeam : Team -> Html Msg
+viewTeam team =
+    let
+        playerNames =
+            List.map .name team.players
+                |> String.join ", "
+
+        teamName =
+            Maybe.withDefault playerNames team.name
+    in
+    div [ class "col" ]
+        [ div [ class "col" ] [ text teamName ]
+        , div [ class "col" ] [ text "1" ]
         ]
 
 
--- scoreboard-frontend/tests/Tests.elm
+++ scoreboard-frontend/tests/Tests.elm
@@ -1,22 +1,59 @@
-module Tests exposing (..)
+module Tests exposing (all)
 
-import Test exposing (..)
 import Expect
+import Main
+import Test exposing (..)
+import Url
+
 
 
 -- Check out http://package.elm-lang.org/packages/elm-community/elm-test/latest to learn more about testing in Elm!
 
 
+type alias RouteTestInput =
+    { url : String
+    , expected : Maybe Main.Route
+    }
+
+
+matchesIndexRouteTestInputs =
+    [ { url = "http://localhost:3000", expected = Just (Main.MatchesIndex Nothing) }
+    , { url = "http://localhost:3000?q=mine", expected = Just (Main.MatchesIndex (Just "mine")) }
+    , { url = "http://localhost:3000/", expected = Just (Main.MatchesIndex Nothing) }
+    , { url = "http://localhost:3000/?q=all", expected = Just (Main.MatchesIndex (Just "all")) }
+    , { url = "http://localhost:3000/matches", expected = Just (Main.MatchesIndex Nothing) }
+    , { url = "http://localhost:3000/matches?q=all", expected = Just (Main.MatchesIndex (Just "all")) }
+    , { url = "http://localhost:3000/matches/?q=all", expected = Just (Main.MatchesIndex (Just "all")) }
+    ]
+
+
+matchesNewRouteTestInputs =
+    [ { url = "http://localhost:3000/matches/new", expected = Just Main.MatchesNew }
+    , { url = "http://localhost:3000/matches/new/", expected = Just Main.MatchesNew }
+    , { url = "http://localhost:3000/matches/new?q=all", expected = Just Main.MatchesNew }
+    , { url = "http://localhost:3000/matches/new/?q=all", expected = Just Main.MatchesNew }
+    ]
+
+
+generateRouteTest : Int -> RouteTestInput -> Test
+generateRouteTest index input =
+    case Url.fromString input.url of
+        Just url ->
+            test (String.fromInt index ++ ". " ++ Url.toString url ++ " routes to appropriate thing") <|
+                \_ ->
+                    Expect.equal input.expected (Main.parseRoute url)
+
+        Nothing ->
+            test "Failed test generation" <|
+                \_ ->
+                    Expect.fail "Failure!"
+
+
 all : Test
 all =
-    describe "A Test Suite"
-        [ test "Addition" <|
-            \_ ->
-                Expect.equal 10 (3 + 7)
-        , test "String.left" <|
-            \_ ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
-            \_ ->
-                Expect.fail "failed as expected!"
+    describe "Routing Test Suite"
+        [ describe "Matches Index Routing"
+            (List.indexedMap generateRouteTest matchesIndexRouteTestInputs)
+        , describe "Matches New Routing"
+            (List.indexedMap generateRouteTest matchesNewRouteTestInputs)
         ]
