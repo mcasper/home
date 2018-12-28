d35037c35c1d30fff676410096c3370b557f326c
Matt Casper <matthewvcasper@gmail.com>
2018-12-28 09:26:34 -0800

    Get docker-compose cluster working again, add recipes

--- recipes/config/routes.rb
+++ recipes/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "recipes#index"
 
   get "/recipes", to: "recipes#index", as: :recipes_root
+  get "/health", to: "health#show"
 
   scope :recipes do
     resources :recipes, only: [:index, :new, :create]
