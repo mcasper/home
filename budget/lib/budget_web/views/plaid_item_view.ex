607e1a04583f5743c3453f725887e1df0ecd412e
Matt Casper <matthewvcasper@gmail.com>
2019-03-11 22:55:00 -0700

    [budget] Configure plaid env in JS by Mix.env

--- budget/lib/budget_web/views/plaid_item_view.ex
+++ budget/lib/budget_web/views/plaid_item_view.ex
@@ -1,3 +1,11 @@
 defmodule BudgetWeb.PlaidItemView do
   use BudgetWeb, :view
+
+  def plaid_env do
+    if Mix.env() == :prod do
+      'development'
+    else
+      'sandbox'
+    end
+  end
 end
