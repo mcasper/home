4e14ab251342ce931bce171c56f03aed5c24471f
Matt Casper <matthewvcasper@gmail.com>
2019-04-07 19:01:08 -0700

    Categorize spend

--- budget/lib/budget_web/controllers/plaid_categorized_transaction_controller.ex
+++ budget/lib/budget_web/controllers/plaid_categorized_transaction_controller.ex
@@ -2,11 +2,13 @@ defmodule BudgetWeb.PlaidCategorizedTransactionController do
   use BudgetWeb, :controller
 
   def create(conn, %{"categorized_transaction" => categorized_transaction_params}) do
+    require IEx; IEx.pry
+    require IEx; IEx.pry
     current_user = current_user(conn)
     create_params = Map.merge(categorized_transaction_params, %{"user_id" => current_user.id})
     case Budget.Plaid.create_categorized_transaction(create_params) do
       {:ok, _categorized_transaction} ->
-        redirect(conn, to: Routes.categorize_spend_path(conn, :index))
+        send_resp(conn, 200, "")
 
       {:error, _changeset} ->
         redirect(conn, to: Routes.categorize_spend_path(conn, :index))
