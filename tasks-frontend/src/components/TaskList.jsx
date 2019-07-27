5efcf0a0ef5bc0e05cb38dc2802d52faef40b0e8
Matt Casper <matthewvcasper@gmail.com>
2019-07-27 09:52:11 -0700

    [tasks-frontend] Get public path working, get basic navbar and body in place

---
+++ tasks-frontend/src/components/TaskList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class TaskList extends React.Component {
+  render() {
+    return(
+      <React.Fragment>
+        <div>Hello!</div>
+      </React.Fragment>
+    )
+  }
+}
+
+export default TaskList
