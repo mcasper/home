0c5380ee30e806f0d64c54e76cc1d1a0c48f5b25
Matt Casper <matthewvcasper@gmail.com>
2019-08-03 09:18:10 -0700

    [tasks-frontend] Add basic task list with dummy data

--- tasks-frontend/src/components/TaskList.jsx
+++ tasks-frontend/src/components/TaskList.jsx
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TaskRow from './TaskRow.jsx';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Container from 'react-bootstrap/Container';
 
-class TaskList extends React.Component {
-  render() {
-    return(
-      <React.Fragment>
-        <div>Hello!</div>
-      </React.Fragment>
-    )
+function TaskList() {
+  const initialTasks = [
+    {
+      id: 1,
+      text: 'Feed the dog',
+      complete: false
+    },
+    {
+      id: 2,
+      text: 'Clean the room',
+      complete: false
+    },
+    {
+      id: 3,
+      text: 'Make dinner',
+      complete: false
+    }
+  ]
+  const [tasks, setTasks] = useState(initialTasks);
+
+  const handleClick = (taskId) => {
+    var newTasks = [];
+    tasks.forEach(function(task) {
+      if (task.id === taskId) {
+        task.complete = true;
+        task.text = (task.text + ' - I was clicked');
+      }
+      newTasks.push(task);
+    });
+    setTasks(newTasks);
   }
+
+  return(
+    <Container>
+      <h1 style={{textAlign: 'center', margin: '35px'}}>Tasks</h1>
+
+      <ListGroup>
+        {tasks.map(task => <TaskRow task={task} clickCallback={handleClick} key={task.id} />)}
+      </ListGroup>
+    </Container>
+  )
 }
 
 export default TaskList
---
+++ tasks-frontend/src/components/TaskRow.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+function TaskRow(props) {
+  const { task, clickCallback } = props;
+
+  return(
+    <ListGroup.Item>
+      <Row>
+        <Col lg={4}>{task.text}</Col>
+        <Col lg={{ span: 4, offset: 4 }}>
+          <Button className={"float-right"} variant="primary" onClick={() => clickCallback(task.id)}>
+            Complete
+          </Button>
+        </Col>
+      </Row>
+    </ListGroup.Item>
+  )
+}
+
+export default TaskRow
