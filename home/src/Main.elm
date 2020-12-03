0b6a838e5eab98772937def2ed27c709fb1248fd
Matt Casper <matthewvcasper@gmail.com>
2020-12-03 11:02:43 -0800

    Include scoreboard in prod

--- home/src/Main.elm
+++ home/src/Main.elm
@@ -28,7 +28,7 @@ developmentSeed =
 
 productionSeed =
     [ { name = "Budget", url = "https://casper.coffee/budget", enabled = True }
-    , { name = "Scoreboard", url = "https://casper.coffee/scoreboard", enabled = False }
+    , { name = "Scoreboard", url = "https://casper.coffee/scoreboard", enabled = True }
     , { name = "Teams", url = "#", enabled = False }
     , { name = "Movies", url = "https://casper.coffee/movies", enabled = True }
     , { name = "Recipes", url = "https://casper.coffee/recipes", enabled = True }
