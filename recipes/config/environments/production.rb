b8a055b7f2c633187eb655744c46c7eb94bf5964
Matt Casper <matthewvcasper@gmail.com>
2021-01-04 09:12:42 -0800

    New encrypted creds with secret_key_base

--- recipes/config/environments/production.rb
+++ recipes/config/environments/production.rb
@@ -1,6 +1,5 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.secret_key_base = ENV.fetch("RECIPES_SECRET_KEY_BASE")
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -17,7 +16,7 @@ Rails.application.configure do
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
-  # config.require_master_key = true
+  config.require_master_key = true
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
